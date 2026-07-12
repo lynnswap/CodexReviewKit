0847dfb595b1a52e24bafbf85133860981048456
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-07-12 23:43:58 +0900

    build(deps): pin subscription lifecycle fix

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let packageDirectory = URL(fileURLWithPath: #filePath)
 let localCodexKitPath = packageDirectory
     .appendingPathComponent("dependencies/CodexKit", isDirectory: true)
     .path
-let codexKitFallbackRevision = "abe61366030e4d7fdf788fae62ada53d21b6cd62"
+let codexKitFallbackRevision = "f5671710ca376aca301de119af27b2c91523ed39"
 let codexKitDependency: Package.Dependency =
     FileManager.default.fileExists(atPath: "\(localCodexKitPath)/Package.swift")
         ? .package(path: localCodexKitPath)
